143e12a8d6a871a925050a9585bbb9d40d8befc9
Stjepan <kekelicstjepan@gmail.com>
2021-12-21 16:24:01 +0100

    initialization

---
+++ ruap_lv6_task1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ruap_lv6_task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
